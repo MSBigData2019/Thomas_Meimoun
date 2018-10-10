701da19058892a718b3bc25e1e864a0bff5063a8
Tomsyy <thomas.meimoun@telecom-paristech.fr>
2018-10-10 19:02:11 +0200

    boucle pour les 3 titres

--- Lesson2/exo_dom_lesson_2.py
+++ Lesson2/exo_dom_lesson_2.py
@@ -8,58 +8,68 @@ Created on Wed Oct 10 13:01:21 2018
 
 import requests
 from bs4 import BeautifulSoup
- 
+
+# balises recherchées
+balise = ['td', 'th', 'span']
+
+#valeurs recherchées
+Stocks = ['LVMH.PA', 'AIR.PA', 'DANO.PA']
+
 Value  = []
 Name   = []
 Header = []
 
-URL = 'https://www.reuters.com/finance/stocks/financial-highlights/LVMH.PA'
+# pattern commun de l'url pour tous les titres
+URLbeg = 'https://www.reuters.com/finance/stocks/financial-highlights/'
 
-req = requests.get(URL)
-#req = requests.get('https://www.reuters.com/finance/stocks/financial-highlights/AIR.PA')
-#req = requests.get('https://www.reuters.com/finance/stocks/financial-highlights/DANO.PA')
-
-soup = BeautifulSoup(req.text, "lxml")
-
-# 'firstHeading'
- 
-for sub_heading in soup.find_all('td'):
-    Value.append(sub_heading.text)
+# boucle le code pour obtenir informations sur chaque titre
+for Titre in Stocks:
+    URL = URLbeg + Titre   
+    req = requests.get(URL)
+    soup = BeautifulSoup(req.text, "lxml")
     
-for sub_heading in soup.find_all('th'):
-    Name.append(sub_heading.text)
+    # récupération des balises dans différentes listes
+    for ba in balise:
+        for sub_head in soup.find_all(ba):
+            if ba == 'td' :
+                Value.append(sub_head.text) 
+            elif ba == 'th' :
+               Name.append(sub_head.text) 
+            else : # ba == 'span':
+               Header.append(sub_head.text)
+               
+    # recherche des valeurs souhaitées 
+
+    Qending = Value[1]
+    ValQend = Value[3].replace(",","") #
+    DivYld = Value[89]
+    CompagnyVal = Value[90] #
+    IndustryVal = Value[91] #
+    SectorVal   = Value[92] #
+    ShareOwned =  Value[293]
+    ValueShareOwn = Value[294].replace("%","") #
     
-for sub_heading in soup.find_all('span'):
-    Header.append(sub_heading.text)
+    Compagny = Name[7]
+    Industry = Name[8]
+    Sector   = Name[9] 
     
-Qending = Value[1]
-ValQend = Value[3].replace(",","") #
-#Mean    = Name[2]
-NameStock = Header[4].strip()
-PriceStock = Header[5].strip() #
-ValueChange = Header[8].strip()[:6]
-Change = str(round(float(Header[12].replace("(","").replace(")","").replace("%","")) / 100, 3))
-ShareOwned =  Value[293]
-ValueShareOwn = Value[294].replace("%","") #
-DivYld = Value[89]
-Compagny = Name[7]
-Industry = Name[8]
-Sector   = Name[9]
-
-CompagnyVal = Value[90] #
-IndustryVal = Value[91] #
-SectorVal   = Value[92] #
-
-print ("about " + NameStock)
-print ("Mean of the " + Qending + " : " + ValQend)
-print (NameStock[:8] + " Price : " + PriceStock)
-print (NameStock[:8] + " change : " + Change + "%")
-print ("Institutionnal " + ShareOwned  + " : " + ValueShareOwn)
-print (DivYld + " \n"   + 
-       Compagny + " : "  + CompagnyVal + " \n" + 
-       Sector + " : "    + SectorVal   + " \n" + 
-       Industry + " : "  + IndustryVal )
-
+    NameStock = Header[4].strip()
+    PriceStock = Header[5].strip() #
+    ValueChange = Header[8].strip()[:6]
+    Change = (Header[12].replace("(","").replace(")","").replace("%","")).strip()
+    
+    # affichage des résultats
+    print ("about " + NameStock)
+    print ("Mean of the " + Qending + " : " + ValQend)
+    print (NameStock[:8] + " Price : " + PriceStock)
+    print (NameStock[:8] + " change : " + Change + "%")
+    print ("Institutionnal " + ShareOwned  + " : " + ValueShareOwn)
+    print (DivYld + ": \n"   + 
+           Compagny + " : "  + CompagnyVal + " \n" + 
+           Sector + " : "    + SectorVal   + " \n" + 
+           Industry + " : "  + IndustryVal )
+    print ('======================================================')
+    
 
 # tout le reste c'est des string, peut être faire une liste avec le snoms à la main ?
 # seulement créer une boucle qui change uniquement le nom des titres
